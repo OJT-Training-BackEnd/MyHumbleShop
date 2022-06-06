74862770d20c5f6f3f25e999c31a7b2e2051cb7c
DatTranLK <thanhdat25062001@gmail.com>
2022-06-06 11:10:59 +0700

    Dat&Quan - Add addToCart and viewCart api

--- MyHumbleShop/Repositories/IUserRepo.cs
+++ MyHumbleShop/Repositories/IUserRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyHumbleShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,7 @@ namespace MyHumbleShop.Repositories
 {
     public interface IUserRepo
     {
+        Task<ServiceResponse<string>> AddToCart(string productId, string userId);
+        Task<ServiceResponse<List<UserCart>>> ViewCart(string userId);
     }
 }
--- MyHumbleShop/Repositories/UserRepo.cs
+++ MyHumbleShop/Repositories/UserRepo.cs
@@ -1,11 +1,111 @@
-﻿using System;
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using MyHumbleShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TikiFake.DatabaseSettings;
+using TikiFake.Models;
+using System.Security.Claims;
+
 
 namespace MyHumbleShop.Repositories
 {
-    public class UserRepo
+    public class UserRepo : IUserRepo
     {
+        private readonly IMongoCollection<Users> _user;
+        private readonly IMongoCollection<Products> _products;
+        private readonly IMongoCollection<RefreshToken> _refreshToken;
+        private readonly IMapper _mapper;
+        private readonly IConfiguration _configuration;
+        private readonly List<RefreshToken> refreshTokens = new List<RefreshToken>();
+
+        public UserRepo(ITakaTikiDatabaseSettings settings,
+                        IMapper mapper,
+                        IConfiguration configuration)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _user = database.GetCollection<Users>(settings.UsersCollectionName);
+            _products = database.GetCollection<Products>(settings.ProductsCollectionName);
+            _refreshToken = database.GetCollection<RefreshToken>(settings.RefreshTokensCollectionName);
+            _mapper = mapper;
+            _configuration = configuration;
+        }
+
+        public async Task<ServiceResponse<string>> AddToCart(string productId, string userId)
+        {
+            var response = new ServiceResponse<string>();
+            var user = await _user.Find(n => n.Id == userId).FirstOrDefaultAsync();
+
+            if (user == null)
+            {
+                response.Success = false;
+                response.Message = "User not found";
+                return response;
+            }
+
+            var productExist = await _products.Find(n => n.Id == productId).FirstOrDefaultAsync();
+            if (productExist == null)
+            {
+                response.Success = false;
+                response.Message = "Product not found";
+                return response;
+            }
+
+            if (user.Cart == null)
+            {
+                user.Cart = new List<UserCart>();
+            }
+
+            foreach (var pro in user.Cart)
+            {
+                if (pro.ProductId == productId)
+                {
+                    pro.Quantiy = (int.Parse(pro.Quantiy) + 1).ToString();
+                    _user.ReplaceOne(n => n.Id == user.Id, user);
+                    response.Success = true;
+                    response.Message = "Add product existed in cart";
+                    return response;
+                }
+            }
+
+            UserCart cart = new UserCart 
+            {
+                ProductId = productId,
+                Quantiy = "1"
+            };
+            user.Cart.Add(cart);
+            _user.ReplaceOne(n => n.Id == user.Id, user);
+            response.Success = true;
+            response.Message = "Add new product to cart";
+            return response;
+        }
+
+        public async Task<ServiceResponse<List<UserCart>>> ViewCart(string userId)
+        {
+            var response = new ServiceResponse<List<UserCart>>();
+            var user = await _user.Find(n => n.Id == userId).FirstOrDefaultAsync();
+            if (user == null)
+            {
+                response.Success = false;
+                response.Message = "User not found";
+                return response;
+            }
+
+            if (user.Cart == null)
+            {
+                response.Success = false;
+                response.Message = "You have no item in the cart";
+                return response;
+            }
+
+            response.Data = user.Cart;
+            response.Success = true;
+            response.Message = "Here is your cart";
+            return response;
+        }
     }
 }
